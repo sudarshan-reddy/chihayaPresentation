d1959387cec5f4d48b40eb4764c35f0a4c031bbf
Sudarshan <sudar.theone@gmail.com>
2017-03-16 19:23:23 +0530

    main: minor changes

--- redis.go
+++ redis.go
@@ -1,8 +1,3 @@
-// START ZREMRANGEBYSCORE
+// START OMIT
 _, err := conn.Do("ZREMRANGEBYSCORE", Key, "-inf", fmt.Sprintf("(%d", time.Now().Add(-s.peerLifetime).Unix()))
-// END ZREMRANGEBYSCORE
-
-// START EXPIRE
-conn.Send("EXPIRE", Key, int(s.peerLifetime.Seconds()))
-// END EXPIRE
-
+// END OMIT
